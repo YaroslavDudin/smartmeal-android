7d38b1cabd4bcb297ca8d18d38bee4a6441fed52
YaroslavDudin <yaroslavedudin@yandex.ru>
2026-03-08 18:43:11 +0300

    ####Fix CI:  final test..............

--- client/SmartMeal/app/src/androidTest/java/com/example/smartmeal/WelcomeScreenTest.kt
+++ client/SmartMeal/app/src/androidTest/java/com/example/smartmeal/WelcomeScreenTest.kt
@@ -5,53 +5,46 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.smartmeal.ui.screens.auth.WelcomeScreen
 import org.junit.Rule
 import org.junit.Test
-import com.example.smartmeal.ui.theme.SmartMealTheme
-
 class WelcomeScreenTest {
+// Тесты работают с API 36.0 и ниже
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
     fun welcomeScreen_displaysTextButtonImage() {
+
         composeTestRule.setContent {
-            SmartMealTheme {
-                WelcomeScreen()
-            }
+            WelcomeScreen()
         }
         composeTestRule
             .onNodeWithTag("food_image")
-            .performScrollTo()
             .assertExists()
 
         composeTestRule
             .onNodeWithText("SmartMeal")
-            .performScrollTo()
             .assertIsDisplayed()
 
         composeTestRule
             .onNodeWithText("Сгенерируйте своё недельное\nменю за пару минут")
-            .performScrollTo()
             .assertExists()
 
         composeTestRule
             .onNodeWithText("Начать")
-            .performScrollTo()
             .assertIsDisplayed()
     }
     @Test
     fun welcomeScreen_buttonClick_callsNavigation() {
+
         var clicked = false
+
         composeTestRule.setContent {
-            SmartMealTheme {
-                WelcomeScreen(
-                    onNavigateNext = { clicked = true }
-                )
-            }
+            WelcomeScreen(
+                onNavigateNext = { clicked = true }
+            )
         }
 
         composeTestRule
             .onNodeWithText("Начать")
-            .performScrollTo()
             .performClick()
 
         assert(clicked)
