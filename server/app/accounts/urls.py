1ba43fd77cb19d9e48fe698c32f0a8dea86ac2e1
Kachulina Tatyana <kachulinatanya@yandex.ru>
2026-03-11 13:29:00 +0300

    add jwt authentication and user registration

---
+++ server/app/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from app.accounts.views import RegisterView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_pair_obtain'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+]
